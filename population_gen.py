4dfed9ebf3df9812bf028f2826cf957951e94caa
william <willpereirabr@gmail.com>
2018-03-07 17:52:16 -0300

    population generator ready

--- population_gen.py
+++ population_gen.py
@@ -1,17 +1,32 @@
 from random import *
 
-typePopulation = input("Escolha uma codificacao 1-BIN, 2-INT, 3-FLO e 4-INTPERM:")
-boundPopulation = input("Escolha uma limite:")
+def populate():
+    for i in range(nPopulation):
+        individual = []
+        if typePopulation == 1:
+            for j in range(cromoPopulation):
+                individual.append(randint(0,1))
+            population.append(individual)
+        if typePopulation == 2:
+            for j in range(cromoPopulation):
+                individual.append(randint(ilPopulation,slPopulation))
+            population.append(individual)
+        if typePopulation == 3:
+            for j in range(cromoPopulation):
+                individual.append(uniform(ilPopulation,slPopulation))
+            population.append(individual)
+        if typePopulation == 4:
+            population.append(sample(range(0,cromoPopulation),cromoPopulation))
+
+typePopulation = input("Escolha uma codificacao 1-BIN, 2-INT, 3-REAL e 4-INTPERM:")
+if typePopulation != 1 and typePopulation != 4:
+    ilPopulation = input("Escolha uma limite inferior:")
+    slPopulation = input("Escolha uma limite superior:")
 nPopulation = input("Escolha a quantidade de individuos:")
 cromoPopulation = input("Escolha o tamanho do cromossomo:")
 
-individual = ""
 population = []
 
-for i in range(nPopulation):
-    if typePopulation == 1:
-        for j in range(cromoPopulation):
-            individual = randint(0,1)
-            population.append(individual)
-        
+populate()
+
 print(population)
