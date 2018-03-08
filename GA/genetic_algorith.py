d2a3b3aac91d14f11a37acc777480486754afa3d
william <willpereirabr@gmail.com>
2018-03-08 16:51:12 -0300

    diversity started, doing real numbers dimension

--- GA/genetic_algorith.py
+++ GA/genetic_algorith.py
@@ -18,6 +18,12 @@ def populate():
         if typePopulation == 4:
             population.append(sample(range(0,cromoPopulation),cromoPopulation))
 
+def diversity():
+    if typePopulation == 3:
+        for i in range (0..cromoPopulation):
+            for j in range(0..nPopulation):
+                population[]
+
 typePopulation = input("Escolha uma codificacao 1-BIN, 2-INT, 3-REAL e 4-INTPERM:")
 if typePopulation != 1 and typePopulation != 4:
     ilPopulation = input("Escolha uma limite inferior:")
@@ -27,6 +33,9 @@ cromoPopulation = input("Escolha o tamanho do cromossomo:")
 
 population = []
 
+#  MAIN LOOP
 populate()
+diversity()
+# END MAIN LOOP
 
 print(population)
